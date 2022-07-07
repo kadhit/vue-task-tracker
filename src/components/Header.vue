6f83296e0568d8007c590c3f17ee35e952420ae2
kadhit <92635388+kadhit@users.noreply.github.com>
2022-07-07 21:26:25 +0700

    created Header component, adding to App

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
