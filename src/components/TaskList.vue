5b21e6411c94e24c5f05725c32564401271a448d
kadhit <92635388+kadhit@users.noreply.github.com>
2022-07-08 13:42:26 +0700

    added TaskList, TaskItem components, connected TaskItem to TaskList

---
+++ src/components/TaskList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <TaskItem :task="task" />
+  </div>
+</template>
+<script>
+import TaskItem from './TaskItem.vue';
+
+export default {
+  name: "TaskList",
+  props: {
+    tasks: {
+      type: Array,
+    },
+  },
+  components: {
+    TaskItem
+  }
+};
+</script>
